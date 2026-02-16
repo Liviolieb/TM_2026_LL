7eb906240849c5ac23c0f61873da2b7ae427e540
Liviolieb <li688315@gmail.com>
2026-02-16 23:49:59 +0100

    surcharge du print

--- cartes.py
+++ cartes.py
@@ -2,14 +2,18 @@ import random
 class cartes :
     Couleurs = ["pique","trefle","carreau","coeur"]
     Valeurs = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-    def __init__ (self):
+    def __init__ (self,couleur=None, valeur=None):
         couleur = self.couleur
         valeur = self.valeur
-    def creer_pack ():
-        L=[]
-        for i in range (13):
-            for j in range (4):
-                x = cartes(x)
-                x.couleur = cartes.Couleurs[j]
-                x.valeur = cartes.Valeurs[i]
-                L.append(x)
+    def __str__(self):
+        return (f"{self.valeur} de {self.couleur} ")
+def creer_pack ():
+    L=[]
+    for i in range (13):
+        for j in range (4):
+            x = cartes(x)
+            x.couleur = cartes.Couleurs[j]
+            x.valeur = cartes.Valeurs[i]
+            L.append(x)
+Liste = creer_pack()
+print (Liste)
