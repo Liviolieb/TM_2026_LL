ce6d26cd389416b5bd0c1c3ee7930e29799afdea
Livilieb <livio@sono.li>
2026-02-17 00:22:07 +0100

    carcasse de classe

---
+++ table.py
@@ -0,0 +1,8 @@
+class table :
+    def __init__(self, flop=None, turn = None, river = None, main_pot = None, side_pot = None):
+        self.flop = flop
+        self.turn = turn
+        self.river = river
+        self.main_pot = main_pot
+        self.side_pot = side_pot
+    
