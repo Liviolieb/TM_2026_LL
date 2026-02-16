0907a80a8926b4def142d5a6613335cc79b653ab
Livilieb <livio@sono.li>
2026-02-17 00:20:35 +0100

    carcasse de classe

--- cartes.py
+++ cartes.py
@@ -1,15 +1,20 @@
 import random
-class cartes :
+class Cartes :
     Couleurs = ["pique","trefle","carreau","coeur"]
-    Valeurs = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-    def __init__ (self):
-        couleur = self.couleur
-        valeur = self.valeur
-    def creer_pack ():
-        L=[]
-        for i in range (13):
-            for j in range (4):
-                x = cartes(x)
-                x.couleur = cartes.Couleurs[j]
-                x.valeur = cartes.Valeurs[i]
-                L.append(x)
+    Valeurs = [2,3,4,5,6,7,8,9,10,"valet","dame","roi","as"]
+    def __init__(self, valeur=None, couleur=None):
+        self.valeur = valeur
+        self.couleur = couleur
+    def __str__ (self):
+        return f"{self.valeur} de {self.couleur}"
+def creer_pack ():
+    L=[]
+    for i in range (13):
+        for j in range (4):
+            x = Cartes()
+            x.couleur = Cartes.Couleurs[j]
+            x.valeur = Cartes.Valeurs[i]
+            print(x)
+            L.append(x)
+    return L
+
