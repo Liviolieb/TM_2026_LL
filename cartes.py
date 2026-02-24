4de51915d1c7e6d44fdbd048403b2a6e6d95b7ea
Liviolieb <li688315@gmail.com>
2026-02-24 22:32:11 +0100

    correction + surrcharge du print

--- cartes.py
+++ cartes.py
@@ -2,9 +2,11 @@ import random
 class Cartes :
     Couleurs = ["pique","trefle","carreau","coeur"]
     Valeurs = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-    def __init__ (self):
-        couleur = self.couleur
-        valeur = self.valeur
+    def __init__ (self,couleur=None,valeur=None):
+        self.couleur = couleur
+        self.valeur = valeur
+    def __str__(self):
+        return f"{self.valeur} de {self.couleur}"
     def creer_pack ():
         L=[]
         for i in range (13):
@@ -13,3 +15,4 @@ class Cartes :
                 x.couleur = Cartes.Couleurs[j]
                 x.valeur = Cartes.Valeurs[i]
                 L.append(x)
+        return L
